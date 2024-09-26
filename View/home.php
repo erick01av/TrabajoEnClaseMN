dadea77b9250ca08ffa9779dd186a7c3f3c8274e
ecalvo92 <ecalvo@crnova.com>
2024-09-25 19:31:03 -0600

    Update home.php

--- View/home.php
+++ View/home.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Sistema Web Miércoles Noche</title>
+  <title>Sistema Web Jueves Noche</title>
   <link rel="shortcut icon" type="image/png" href="images/seodashlogo.png" />
   <link rel="stylesheet" href="css/styles.min.css" />
 </head>
